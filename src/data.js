443735e803f5196273e81689737227b0678e5dcb
jenifercoila <jeniferfcr@gmail.com>
2018-12-19 20:19:35 -0500

    Primer commit, funcion con uso de templates, html muy basico con header, background en css

--- src/data.js
+++ src/data.js
@@ -1,8 +1,15 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = (arr) => {
+//   const arrKeys = Object.keys(data)
+//   for(let i = 0; i < arrKeys.length; i++){
+//     //console.log(arrKeys[i])
+//     console.log(data[arrKeys[i]])
+// }
+//   return example;
+// };
 
-window.example = example;
+// window.example = example;
+
+
---
+++ src/main.js
@@ -0,0 +1,41 @@
+
+let containerCards = document.getElementById('lista-de-los-campeones');
+
+const arrKeys = Object.values(LOL.data);
+
+let functionChampions = (championsData) => {
+  // let championsName = Object.keys(datas);  
+  // let championsData = Object.values(datas);
+  let championsCard = '';
+
+  championsData.forEach((data) => {
+    const list = `
+  <a class ="card-button" href="#champions-modal">
+  <input type="image" src="${data.img}"/>
+  <h1>${data.name}</h1>
+  <h3> ${data.title}</h3>
+  <h3>Ataque ${data.info.attack}</h3>
+  <h3>Defensa ${data.info.defense}</h3>
+  <h3>Magia ${data.info.magic}</h3>
+  <h3>Dificultad ${data.info.difficulty}</h3>
+  </a>
+
+<div id="champions-modal" class="champions-modal">
+  <img class="modal-image" src="${data.splash}" />
+</div>
+`;
+    championsCard += list;
+    containerCards.innerHTML = championsCard;
+  });
+
+  return championsCard;
+};
+
+functionChampions(arrKeys);
+
+// document.getElementById("button-card").addEventListener("click", function(){
+
+
+window.LOL = {
+  functionChampions,
+} 
