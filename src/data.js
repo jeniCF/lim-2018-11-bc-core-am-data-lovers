161a7c33b3a7a8ee7e142010b17685a9d2e3ab06
jenifercoila <jeniferfcr@gmail.com>
2019-01-04 17:05:52 -0500

    Funcion filtrado que obtiene el id, box modal sin index

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-esta es una función de ejemplo
+/* esta es una función de ejemplo
 puedes ver como agregamos la función a nuestro objeto global window
 
  const example = (arr) => {
@@ -10,6 +10,38 @@ console.log(arrKeys[i])
 return example;
 };
 
-window.example = example;
+window.example = example; */
+
+const checkbox = Object.values(document.getElementsByClassName('checkbox'));
+console.log(checkbox);
+
+let selectedTags= [];
+
+checkbox.forEach((tag) =>{
+  tag.addEventListener ('change', () => {
+    if (event.target.checked) {
+      selectedTags.push(tag.value);
+    
+    }
+    else {
+      x= selectedTags.indexOf(tag.value)
+      selectedTags.splice(x,1)
+      functionChampions(arrKeys);
+    }
+  })
+})
 
 
+const dataCopy = Object.assign({},LOL.data) 
+const arrayCopy = Object.values(dataCopy);
+
+
+const dataFilter = (data, condition) => {
+ return data.filter ((eleTags) => {
+  return eleTags.tags.includes(condition) === true;
+ });
+};
+
+dataFilter(arrayCopy, 'Mage').forEach((ele)=>{
+  console.log('result = '+ ele.id);
+});
--- src/main.js
+++ src/main.js
@@ -1,48 +1,39 @@
-
 let containerCards = document.getElementById('champions-cards-group');
 let containerModalsCards = document.getElementById('champions-modal');
-let modal = document.getElementById('champions-modal');
 
 
 const arrKeys = Object.values(LOL.data);
 
+
 let functionChampions = (championsData) => {
   // let championsName = Object.keys(datas);  
   // let championsData = Object.values(datas);
-  let championsCard = '';
-  let championsModal = '';
+  let championsCard = [];
+  let championsModal = [];
 
-  championsData.forEach((data, index) => {
-    const list = `
+  championsData.forEach((data) => {
+    championsCard.push(`
   <a class ='champion-card' href='#champions-modal'>
   <input type='image' src='${data.img}'/>
   <h1>${data.name}</h1> 
-  </a> `;
-    
-    const modal = `
-    <img class='modal-image' id= '${index}' src='${data.splash}' />
-  `;
+  </a> `);
 
-    championsCard += list;
-    championsModal += modal;
+    championsModal.push(`
+    <img class='modal-splash' src='${data.splash}' id='${data.id}' />
+  `);
 
     containerCards.innerHTML = championsCard;
     containerModalsCards.innerHTML = championsModal;
   });
-
   return championsCard, championsModal;
 };
-
 functionChampions(arrKeys);
 
-// document.getElementById('button-card').addEventListener('click', function(){
+containerCards.addEventListener('click', myFunction);
 
-
-window.LOL = {
-  functionChampions,
-} 
-
-let imgModal= document.getElementById('modal-image')
+function myFunction() {
+  containerModalsCards.style.display = 'block';
+}
 
 /* 
   <h3> ${data.title}</h3>
@@ -51,15 +42,3 @@ let imgModal= document.getElementById('modal-image')
 <h3>Defensa ${data.info.defense}</h3>
 <h3>Magia ${data.info.magic}</h3>
 <h3>Dificultad ${data.info.difficulty}</h3>*/
-    
-const filterType = (data, type) => {
-  const newData = data.forEach(copyData => 
-    Object.assign({},
-  { name: copyData.name, tags: copyData.tags, img: copyData.img }));
-  const filterTank = newData.filter((data) => {
-    return data.tags.includes(type) === true;
-  });
-  return filterTank;
-};
-
-filterType(LOL.data, 'Tank');
