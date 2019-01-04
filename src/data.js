063f9acb3f05af01c94e2ea5579b2f671724cd4e
jenifercoila <jeniferfcr@gmail.com>
2019-01-04 16:59:33 -0500

    Función filterType

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+esta es una función de ejemplo
+puedes ver como agregamos la función a nuestro objeto global window
 
-// const example = (arr) => {
-//   const arrKeys = Object.keys(data)
-//   for(let i = 0; i < arrKeys.length; i++){
-//     //console.log(arrKeys[i])
-//     console.log(data[arrKeys[i]])
-// }
-//   return example;
-// };
+ const example = (arr) => {
+       const arrKeys = Object.keys(data)
+   for(let i = 0; i < arrKeys.length; i++){
+console.log(arrKeys[i])
+     console.log(data[arrKeys[i]])
+}
+return example;
+};
 
-// window.example = example;
+window.example = example;
 
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 
 let containerCards = document.getElementById('champions-cards-group');
+let containerModalsCards = document.getElementById('champions-modal');
+let modal = document.getElementById('champions-modal');
+
 
 const arrKeys = Object.values(LOL.data);
 
@@ -9,38 +12,54 @@ let functionChampions = (championsData) => {
   let championsCard = '';
   let championsModal = '';
 
-  championsData.forEach((data) => {
+  championsData.forEach((data, index) => {
     const list = `
-  <a class ="champion-card" href="#champions-modal">
-  <input type="image" src="${data.img}"/>
+  <a class ='champion-card' href='#champions-modal'>
+  <input type='image' src='${data.img}'/>
   <h1>${data.name}</h1> 
   </a> `;
     
+    const modal = `
+    <img class='modal-image' id= '${index}' src='${data.splash}' />
+  `;
+
     championsCard += list;
+    championsModal += modal;
+
     containerCards.innerHTML = championsCard;
+    containerModalsCards.innerHTML = championsModal;
   });
 
-  return championsCard;
+  return championsCard, championsModal;
 };
 
 functionChampions(arrKeys);
 
-// document.getElementById("button-card").addEventListener("click", function(){
+// document.getElementById('button-card').addEventListener('click', function(){
 
 
 window.LOL = {
   functionChampions,
 } 
 
-/* let containerModalsCards = document.getElementById('champions-modal');
+let imgModal= document.getElementById('modal-image')
+
+/* 
   <h3> ${data.title}</h3>
 
 <h3>Ataque ${data.info.attack}</h3>
 <h3>Defensa ${data.info.defense}</h3>
 <h3>Magia ${data.info.magic}</h3>
-<h3>Dificultad ${data.info.difficulty}</h3>
-    championsModal += modal;
-    const modal = `
-    <img class="modal-image" src="${data.splash}" />
-  `;
-containerModalsCards.innerHTML = championsModal; */
+<h3>Dificultad ${data.info.difficulty}</h3>*/
+    
+const filterType = (data, type) => {
+  const newData = data.forEach(copyData => 
+    Object.assign({},
+  { name: copyData.name, tags: copyData.tags, img: copyData.img }));
+  const filterTank = newData.filter((data) => {
+    return data.tags.includes(type) === true;
+  });
+  return filterTank;
+};
+
+filterType(LOL.data, 'Tank');
